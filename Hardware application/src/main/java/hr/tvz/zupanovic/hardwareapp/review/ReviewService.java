02ff2bfe45a4d953fe3e601b357e5c032fe07c0f
tinzupanovic <106931403+tinzupanovic@users.noreply.github.com>
2022-06-09 11:24:38 +0200

    Delete Hardware application directory

--- Hardware application/src/main/java/hr/tvz/zupanovic/hardwareapp/review/ReviewService.java
+++
@@ -1,9 +0,0 @@
-package hr.tvz.zupanovic.hardwareapp.review;
-
-import java.util.List;
-
-public interface ReviewService {
-    List<ReviewDTO> findAll();
-
-    List<ReviewDTO> findByhardware(int id);
-}
